bea0583024b7f0b86e57023f65de37071c5ab6c8
James Wunder <Jwunder2@mail.greenriver.edu>
2021-06-03 19:01:17 -0400

    added routing to index.php and basic comments/placeholder text for home.html and created workoutype.html

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
 <?php
+//turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+require_once ('model/data-layer.php');
+require_once ('model/validation.php');
+
+//Instantiate the base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET|POST /Home', function($f3){
+
+    //create data-layer and validation for login to workout tracker on home.html
+
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+
+$f3->route('GET|POST /', function($f3){
+
+    //
+
+    $view = new Template();
+    echo $view->render('views/workoutType.html');
+});
+
+//Run fat free
+$f3->run();
